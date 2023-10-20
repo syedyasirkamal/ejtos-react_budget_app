1d0c63d55e6d4b4aad7e4a1f83a8fadd2062c7ea
syedyasirkamal <syedyasirkamal@gmail.com>
2023-10-19 21:55:32 -0400

    second commit

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining,currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -58,13 +58,15 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <span style={{ fontSize:'24px', marginLeft: '2rem'}}>
+                  {currency}
+                  </span>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '0.4rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,51 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const updateCurrency = (value) => {
+        dispatch( {
+            type: "CHG_CURRENCY",
+            payload: value
+        })
+    }
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        if(newBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than spending");
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency}  
+
+
+</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+
+<select name="Location" id="Location"
+                style={{
+                    background: "#a5e1a0",
+                    border: 0,
+                    color: "white",
+                    outline: 0,
+                    fontSize: "14px"
+                }}
+                onChange={ (event) => updateCurrency(event.target.value) }
+            >
+                <option value="$">Currency ($ Dollar)</option>
+                <option value="£" selected>Currency (£ Pound)</option>
+                <option value="€">Currency (€ Euro)</option>
+                <option value="₹">Currency (₹ Ruppee)</option>
+            </select>
+
 </div>
+
     );
 };
 export default Budget;
